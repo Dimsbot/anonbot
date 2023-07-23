dbd69c7719b67414ab132472fbec42785ee03cf7
Dimsbot <106795604+Dimsbot@users.noreply.github.com>
2023-07-23 22:00:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 TOKEN = os.environ.get("TOKEN", "6236915504:AAFwP73_xAVbYtpktc3rWr8MzBVFdTEMrDM") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "sansdimz") # jangan dirubah agar tidak error
 GROUP = os.environ.get("GROUP", "dimzbotzzxyz") # 
-CHANNEL = os.environ.get("CHANNEL", "dimzgz") # 
+CHANNEL = os.environ.get("CHANNEL", "supdimztelebot") # 
